6212545e74180d151c7fe6d559b9207bd6682210
FrontierUI <uifrontier95@gmail.com>
2023-08-12 08:01:10 -0700

    communities

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -5,6 +5,8 @@ import Searchbar from '@/components/shared/Searchbar';
 
 import { fetchUser } from '@/lib/actions/user.actions';
 import { fetchCommunities } from '@/lib/actions/community.actions';
+import CommunityCard from '@/components/cards/CommunityCard';
+import Pagination from '@/components/shared/Pagination';
 
 const Page = async ({
   searchParams,
@@ -31,7 +33,31 @@ const Page = async ({
         <Searchbar routeType="communities" />
       </div>
 
-      <section className="mt-9 flex flex-wrap gap-4"></section>
+      <section className="mt-9 flex flex-wrap gap-4">
+        {result.communities.length === 0 ? (
+          <p className="no-result">No Result</p>
+        ) : (
+          <>
+            {result.communities.map((community) => (
+              <CommunityCard
+                key={community.id}
+                id={community.id}
+                name={community.name}
+                username={community.username}
+                imgUrl={community.image}
+                bio={community.bio}
+                members={community.members}
+              />
+            ))}
+          </>
+        )}
+      </section>
+
+      <Pagination
+        path="communities"
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
     </>
   );
 };
